7bafc60f5c2bea137c2ba8bbcedab0d483bf0c4c
lvcoc <742818396@qq.com>
2019-03-21 15:45:24 +0800

    first

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+/// <summary>
+/// 空白，实体
+/// </summary>
+public enum CellType {
+	Blank,
+	Solid
+}
+/// <summary>
+/// 流向 上右下左↑→↓←
+/// </summary>
+public enum FlowDirection { 
+	Top = 0, 
+	Right = 1,
+	Bottom = 2, 
+	Left = 3
+}
+
+public class Cell : MonoBehaviour {
+    
+	// Grid index reference
+	public int X { get ; private set; }
+	public int Y { get; private set; }
+
+	// Amount of liquid in this cell
+	public float Liquid { get; set; }
+
+	// Determines if Cell liquid is settled
+	private bool _settled;
+	public bool Settled { 
+		get { return _settled; } 
+		set {
+			_settled = value; 
+			if (!_settled) {
+				SettleCount = 0;
+			}
+		}
+	}
+	public int SettleCount { get; set; }
+
+	public CellType Type { get; private set; }
+
+	// 邻居
+	public Cell Top;
+	public Cell Bottom { get; set; }
+	public Cell Left { get; set; }
+	public Cell Right { get; set; }
+
+	// Shows flow direction of cell
+	public int Bitmask { get; set; }
+	public bool[] FlowDirections = new bool[4];
+
+	// 水体颜色
+	public Color Color;
+	public Color DarkColor = new Color (0, 0.1f, 0.2f, 1);
+
+	SpriteRenderer BackgroundSprite;
+	SpriteRenderer LiquidSprite;
+	SpriteRenderer FlowSprite;
+
+	Sprite[] FlowSprites;
+
+	bool ShowFlow;
+	bool RenderDownFlowingLiquid;
+	bool RenderFloatingLiquid;
+
+	void Awake() {
+		BackgroundSprite = transform.Find ("Background").GetComponent<SpriteRenderer> ();
+		LiquidSprite = transform.Find ("Liquid").GetComponent<SpriteRenderer> ();
+		FlowSprite = transform.Find ("Flow").GetComponent<SpriteRenderer> ();
+		//Color = LiquidSprite.color;
+	}
+
+	public void Set(int x, int y, Vector2 position, float size, Sprite[] flowSprites, bool showflow, bool renderDownFlowingLiquid, bool renderFloatingLiquid) {
+		
+		X = x;
+		Y = y;
+
+		RenderDownFlowingLiquid = renderDownFlowingLiquid;
+		RenderFloatingLiquid = renderFloatingLiquid;
+		ShowFlow = showflow;
+		FlowSprites = flowSprites;
+		transform.position = position;
+		transform.localScale = new Vector2 (size, size);
+
+		FlowSprite.sprite = FlowSprites [0];
+	}
+	/// <summary>
+    /// 设置格子类型
+    /// </summary>
+    /// <param name="type"></param>
+	public void SetType(CellType type) {
+		Type = type;
+		if (Type == CellType.Solid) {
+			Liquid = 0;
+		}
+		UnsettleNeighbors ();
+	}
+    /// <summary>
+    /// 添加水
+    /// </summary>
+    /// <param name="amount"></param>
+	public void AddLiquid(float amount) {
+		Liquid += amount;
+		Settled = false;
+	}
+
+	public void ResetFlowDirections() {
+		FlowDirections [0] = false;
+		FlowDirections [1] = false;
+		FlowDirections [2] = false;
+		FlowDirections [3] = false;
+	}
+
+	// Force neighbors to simulate on next iteration
+	public void UnsettleNeighbors() {
+		if (Top != null)
+			Top.Settled = false;
+		if (Bottom != null)
+			Bottom.Settled = false;
+		if (Left != null)
+			Left.Settled = false;
+		if (Right != null)
+			Right.Settled = false;
+	}
+
+	public void Update() {
+
+		// Set background color based on cell type
+		if (Type == CellType.Solid) {
+			BackgroundSprite.color = Color.gray;
+		} else {
+			BackgroundSprite.color = Color.black;
+		}
+
+		// Update bitmask based on flow directions
+		Bitmask = 0;
+		if (FlowDirections [(int)FlowDirection.Top])
+			Bitmask += 1;
+		if (FlowDirections [(int)FlowDirection.Right])
+			Bitmask += 2;
+		if (FlowDirections [(int)FlowDirection.Bottom])
+			Bitmask += 4;
+		if (FlowDirections [(int)FlowDirection.Left])
+			Bitmask += 8;
+		
+		if (ShowFlow) {
+			// 显示方向
+			FlowSprite.sprite = FlowSprites [Bitmask];
+		} else {
+			FlowSprite.sprite = FlowSprites [0];
+		}
+
+		//根据水的数量调整图片大小
+		LiquidSprite.transform.localScale = new Vector2 (1, Mathf.Min (1, Liquid));	
+
+		// Optional rendering flags
+		if (!RenderFloatingLiquid) {
+			// 如果下面有残留的水，清空图片
+			if (Bottom != null && Bottom.Type != CellType.Solid && Bottom.Liquid <= 0.99f) {
+				LiquidSprite.transform.localScale = new Vector2 (0, 0);	
+			}
+		}
+		if (RenderDownFlowingLiquid) {
+			// 如果上面有水，填满一个格子
+			if (Type == CellType.Blank && Top != null && (Top.Liquid > 0.05f || Top.Bitmask == 4)) {
+				LiquidSprite.transform.localScale = new Vector2 (1, 1);	
+			}
+		}
+
+		// 根据水的数量绘制颜色
+		LiquidSprite.color = Color.Lerp (Color, DarkColor, Liquid / 4f);
+	}
+
+}
---
+++ Assets/Scripts/Liquid.cs
@@ -0,0 +1,252 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Liquid : MonoBehaviour {
+
+    [Header("最大最小液体体积")]
+    public float MaxValue = 1.0f;
+    public float MinValue = 0.005f;
+
+    [Header("液体上方可以残留的最大液体量")]
+    public float MaxCompression = 0.25f;
+
+    [Header("每次迭代允许流动的最低和最高液体量")]
+    public float MinFlow = 0.005f;
+    public float MaxFlow = 4f;
+
+    [Header("基础速度")]
+    [Range(0f,1f)]
+	public float FlowSpeed = 1f;
+
+	// 跟踪液体数组
+	float[,] Diffs;
+    /// <summary>
+    /// 初始化液体数组
+    /// </summary>
+    /// <param name="cells"></param>
+	public void Initialize(Cell[,] cells) {
+		Diffs = new float[cells.GetLength (0), cells.GetLength (1)];
+	}
+
+    /// <summary>
+    /// 下面是计算如何在两个垂直相邻的单元之间分配给定水量的函数
+    /// </summary>
+    /// <param name="remainingLiquid"></param>
+    /// <param name="destination"></param>
+    /// <returns></returns>
+    float CalculateVerticalFlowValue(float remainingLiquid, Cell destination)
+	{
+		float sum = remainingLiquid + destination.Liquid;
+		float value = 0;
+        //如果总量小于最大值，直接流动最大值,话说为什么是返回最大值
+		if (sum <= MaxValue) {
+			value = MaxValue;
+
+		} else if (sum < 2 * MaxValue + MaxCompression) {
+			value = (MaxValue * MaxValue + sum * MaxCompression) / (MaxValue + MaxCompression);
+		} else {
+			value = (sum + MaxCompression) / 2f;
+		}
+
+		return value;
+	}
+
+    //模拟流动
+	public void Simulate(ref Cell[,] cells) {
+
+		float flow = 0;
+
+		// 清空流体数组
+		for (int x = 0; x < cells.GetLength (0); x++) {
+			for (int y = 0; y < cells.GetLength (1); y++) {
+				Diffs [x, y] = 0;
+			}
+		}
+
+		// 主循环
+		for (int x = 0; x < cells.GetLength(0); x++) {
+			for (int y = 0; y < cells.GetLength(1); y++) {
+
+				//从数组拿到细胞，清空方向
+				Cell cell = cells [x, y];
+				cell.ResetFlowDirections ();
+
+				//判断格子类型
+				if (cell.Type == CellType.Solid) {
+					cell.Liquid = 0;
+					continue;
+				}
+                //没有液体
+				if (cell.Liquid == 0)
+					continue;
+                //如果已经稳定
+				if (cell.Settled) 
+					continue;
+                //如果小于最小值
+				if (cell.Liquid < MinValue) {
+					cell.Liquid = 0;
+					continue;
+				}
+
+				//跟踪当前格子的液体
+				float startValue = cell.Liquid;
+				float remainingValue = cell.Liquid;
+				flow = 0;
+
+                Button(cell,x,y,ref flow,ref remainingValue);
+				// 小于最小值直接抹掉
+				if (remainingValue < MinValue) {
+					Diffs [x, y] -= remainingValue;
+					continue;
+				}
+
+                Left(cell, x, y, ref flow, ref remainingValue);
+                // 小于最小值直接抹掉
+                if (remainingValue < MinValue) {
+					Diffs [x, y] -= remainingValue;
+					continue;
+				}
+
+                Right(cell, x, y, ref flow, ref remainingValue);
+                // 小于最小值直接抹掉
+                if (remainingValue < MinValue) {
+					Diffs [x, y] -= remainingValue;
+					continue;
+				}
+
+                Top(cell, x, y, ref flow, ref remainingValue);
+                // 小于最小值直接抹掉
+                if (remainingValue < MinValue) {
+					Diffs [x, y] -= remainingValue;
+					continue;
+				}
+
+				// 如果不在流动，就设为稳定状态
+				if (startValue == remainingValue) {
+					cell.SettleCount++;
+					if (cell.SettleCount >= 10) {
+						cell.ResetFlowDirections ();
+						cell.Settled = true;
+					}
+				} else {
+                    //向周围格子流动
+					cell.UnsettleNeighbors ();
+				}
+			}
+		}
+			
+		//更新格子数组的液体
+		for (int x = 0; x < cells.GetLength (0); x++) {
+			for (int y = 0; y < cells.GetLength (1); y++) {
+				cells [x, y].Liquid += Diffs [x, y];
+				if (cells [x, y].Liquid < MinValue) {
+					cells [x, y].Liquid = 0;
+					cells [x, y].Settled = false;	//空格子都是可以流动的
+				}				
+			}
+		}			
+	}
+    private void Button(Cell cell, int x,int y,ref float flow,ref float remainingValue)
+    {
+        if (cell.Bottom != null && cell.Bottom.Type == CellType.Blank)
+        {
+
+            // 计算下落速度
+            flow = CalculateVerticalFlowValue(cell.Liquid, cell.Bottom) - cell.Bottom.Liquid;
+            if (cell.Bottom.Liquid > 0 && flow > MinFlow)
+                flow *= FlowSpeed;
+
+            //约束速度
+            flow = Mathf.Max(flow, 0);
+            if (flow > Mathf.Min(MaxFlow, cell.Liquid))
+                flow = Mathf.Min(MaxFlow, cell.Liquid);
+
+            // 更新数组
+            if (flow != 0)
+            {
+                remainingValue -= flow;
+                Diffs[x, y] -= flow;
+                Diffs[x, y + 1] += flow;
+                cell.FlowDirections[(int)FlowDirection.Bottom] = true;
+                cell.Bottom.Settled = false;
+            }
+        }
+    }
+    private void Left(Cell cell, int x, int y, ref float flow, ref float remainingValue)
+    {
+        if (cell.Left != null && cell.Left.Type == CellType.Blank)
+        {
+
+            // 计算速度
+            flow = (remainingValue - cell.Left.Liquid) / 4f;
+            if (flow > MinFlow)
+                flow *= FlowSpeed;
+
+            // 约束
+            flow = Mathf.Max(flow, 0);
+            if (flow > Mathf.Min(MaxFlow, remainingValue))
+                flow = Mathf.Min(MaxFlow, remainingValue);
+
+            // 更新
+            if (flow != 0)
+            {
+                remainingValue -= flow;
+                Diffs[x, y] -= flow;
+                Diffs[x - 1, y] += flow;
+                cell.FlowDirections[(int)FlowDirection.Left] = true;
+                cell.Left.Settled = false;
+            }
+        }
+    }
+    private void Right(Cell cell, int x, int y, ref float flow, ref float remainingValue)
+    {
+        if (cell.Right != null && cell.Right.Type == CellType.Blank)
+        {
+
+            //计算速度
+            flow = (remainingValue - cell.Right.Liquid) / 4f;
+            if (flow > MinFlow)
+                flow *= FlowSpeed;
+
+            // 约束
+            flow = Mathf.Max(flow, 0);
+            if (flow > Mathf.Min(MaxFlow, remainingValue))
+                flow = Mathf.Min(MaxFlow, remainingValue);
+
+            // 更新
+            if (flow != 0)
+            {
+                remainingValue -= flow;
+                Diffs[x, y] -= flow;
+                Diffs[x + 1, y] += flow;
+                cell.FlowDirections[(int)FlowDirection.Right] = true;
+                cell.Right.Settled = false;
+            }
+        }
+    }
+    private void Top(Cell cell, int x, int y, ref float flow, ref float remainingValue)
+    {
+        if (cell.Top != null && cell.Top.Type == CellType.Blank)
+        {
+            //计算速度
+            flow = remainingValue - CalculateVerticalFlowValue(remainingValue, cell.Top);
+            if (flow > MinFlow)
+                flow *= FlowSpeed;
+
+            // 约束
+            flow = Mathf.Max(flow, 0);
+            if (flow > Mathf.Min(MaxFlow, remainingValue))
+                flow = Mathf.Min(MaxFlow, remainingValue);
+
+            // 更新液体数组
+            if (flow != 0)
+            {
+                remainingValue -= flow;
+                Diffs[x, y] -= flow;
+                Diffs[x, y - 1] += flow;
+                cell.FlowDirections[(int)FlowDirection.Top] = true;
+                cell.Top.Settled = false;
+            }
+        }
+    }
+}
